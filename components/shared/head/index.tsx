47942876187bb546584545b8667ca86b47f99c8b
davidchanho <33166931+davidchanho@users.noreply.github.com>
2021-03-25 12:09:10 -0400

    add lang to html

--- components/shared/head/index.tsx
+++ components/shared/head/index.tsx
@@ -9,7 +9,6 @@ function Head() {
     <NHead>
       <title>{name}</title>
       <link rel="icon" href="/favicon.ico" />
-      <meta lang="en" />
       <meta
         name="description"
         content="All you can eat Mongolian styled stir fry in Fayetteville, NC"
